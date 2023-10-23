79117489f7f5959b8030c50f48a759c496d3856e
akashaher <akashaher27@gmail.com>
2023-10-23 10:11:53 +0530

    Ui-Common:Added Messenger Icon

--- ui-common/src/main/java/com/example/ui_common/customView/Icon.kt
+++ ui-common/src/main/java/com/example/ui_common/customView/Icon.kt
@@ -9,8 +9,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.nativeCanvas
@@ -83,8 +85,51 @@ fun FaceBookIcon() {
     }
 }
 
+@Composable
+fun MessengerIcon() {
+    val colors = listOf(Color(0xFF02b8f9), Color(0xFF0277fe))
+    Canvas(
+        modifier = Modifier
+            .size(grid_x12_5)
+            .padding(grid_x2)
+    ) {
+
+        drawOval(
+            brush = Brush.verticalGradient(colors),
+            size = Size(size.width, size.height * 0.95f)
+        )
+        val trianglePath = Path().let {
+            it.moveTo(this.size.width * .20f, this.size.height * .77f)
+            it.lineTo(this.size.width * .20f, this.size.height * 0.95f)
+            it.lineTo(this.size.width * .37f, this.size.height * 0.86f)
+            it.close()
+            it
+        }
+
+        val electricPath = Path().let {
+            it.moveTo(this.size.width * .20f, this.size.height * .60f)
+            it.lineTo(this.size.width * .48f, this.size.height * .35f)
+            it.lineTo(this.size.width * .55f, this.size.height * .45f)
+            it.lineTo(this.size.width * .80f, this.size.height * .30f)
+            it.lineTo(this.size.width * .48f, this.size.height * .58f)
+            it.lineTo(this.size.width * .40f, this.size.height * .50f)
+            it.close()
+            it
+        }
+
+        drawPath(
+            path = trianglePath,
+            brush = Brush.verticalGradient(colors),
+        )
+        drawPath(
+            path = electricPath,
+            color = Color.White
+        )
+    }
+}
+
 @Preview
 @Composable
 fun Preview() {
-    FaceBookIcon()
+    MessengerIcon()
 }
